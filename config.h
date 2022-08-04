2c7dd74bbf1ec8910898d3d96ef95288b7e199aa
Elaina Bialkowski <elainabialkowski@outlook.com>
2022-08-04 16:27:27 -0400

    backing up before trying gentoo

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hasklug Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
